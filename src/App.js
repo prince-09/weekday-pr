21879dd663bdb38cb4b846a3a3df995fd0c6feda
prince-smiles <prince@doc32.in>
2024-05-07 23:26:53 +0530

    infinite scroll done

--- src/App.js
+++ src/App.js
@@ -2,20 +2,61 @@ import logo from './logo.svg';
 import './App.css';
 import { Card, CardContent, Grid } from '@mui/material';
 import CardDetails from './components/CardDetails';
+import { useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 function App() {
+
+  const [jobs, setJobs] = useState([]);
+  const [offset, setOffset] = useState(0);
+
+  async function postData() {
+    const response = await fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", {
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        "limit": 10,
+        "offset": offset
+      }), // body data type must match "Content-Type" header
+    });
+    const jobList = (await response.json())?.jdList;
+    const filteredList = jobList.filter((item) => item.companyName && item.jobDetailsFromCompany && item.jobRole && item.location && item.logoUrl && item.maxExp && item.maxJdSalary && item.minExp && item.minJdSalary)
+    console.log("JO ------ ", filteredList)
+    setJobs([...jobs, ...filteredList]);
+    setOffset(offset+10);
+    // return response.json(); // parses JSON response into native JavaScript objects
+  };
+
+  postData();
 
   const arr = [{}, {}, {}, {}, {}]
   return (
     <div className="App">
       <header className="App-header">
-        <Grid container spacing={3} style={{ padding: 20, width: "85%"}}>
-          {arr.map((item) => (
-            <Grid item xs={4}>
-              <CardDetails />
-            </Grid>
-          ))}
-        </Grid>
+        <InfiniteScroll
+          dataLength={10} //This is important field to render the next data
+          next={postData}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: 'center' }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+        >
+          <Grid container spacing={3} style={{ padding: 20, width: "85%"}}>
+            {jobs.map((item) => (
+              <Grid item xs={4}>
+                <CardDetails jobDetails={item}/>
+              </Grid>
+            ))}
+          </Grid>
+        </InfiniteScroll>
       </header>
     </div>
   );
--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -1,29 +1,29 @@
-import { Button, Card, CardActions, CardContent, Chip, Paper, Typography } from "@mui/material";
+import { Button, capitalize, Card, CardActions, CardContent, Chip, Paper, Typography } from "@mui/material";
+import { green, grey } from "@mui/material/colors";
 
-export default function CardDetails(){
+export default function CardDetails({ jobDetails }){
+    const {companyName, jobDetailsFromCompany, jobRole, location, logoUrl, maxExp, maxJdSalary, minExp, minJdSalary} = jobDetails;
     return (
         <Card variant="outlined" sx={{ boxShadow: 2}} style={{ borderRadius: 20}}>
             <CardContent style={{ display: "flex", flexDirection: "column", alignItems: "start", padding: 15 }}>
                 <Chip label="⌛ Posted 10 days ago" />
                 <div style={{ display: "flex", flexDirection: "row", marginTop: 10, gap: 8}}>
-                    <img src="https://unsplash.com/photos/green-leafed-plant-on__f0tEf_E" style={{ height: "30px", marginTop: "10px", marginLeft: "4px"}} />
+                    <img src={logoUrl} style={{ height: "30px", marginTop: "10px", marginLeft: "4px"}} />
                     <div style={{ display: "flex", flexDirection: "column", alignItems: "start"}}>
-                        <Typography variant="caption">Fampay</Typography>
-                        <Typography variant="body2">Software Engineer</Typography>
-                        <Typography variant="caption">Delhi</Typography>
+                        <Typography variant="caption">{companyName}</Typography>
+                        <Typography variant="body2">{jobRole}</Typography>
+                        <Typography variant="caption">{location}</Typography>
                     </div>
                 </div>
-                <Typography variant="body2">Estimated Salary: ₹18LPA - ₹30LPA</Typography>
-                <Typography variant="body2">About Company:</Typography>
-                <Typography variant="caption">About Us:</Typography>
-                <Typography variant="caption" style={{ textAlign: "start"}}>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</Typography>
+                <Typography variant="body2">Estimated Salary: ₹{minJdSalary}LPA - ₹{maxJdSalary}LPA</Typography>
+                <Typography variant="body2" sx={{ fontWeight: 'bold', color: grey[800]}}>About Company:</Typography>
+                <Typography variant="caption" sx={{fontWeight: 'medium', color: grey[600]}}>About Us:</Typography>
+                <Typography variant="caption" style={{ textAlign: "start"}}>{jobDetailsFromCompany}</Typography>
                 <Typography variant="caption">Min experience</Typography>
-                <Typography variant="body2">2 years</Typography>
-                <Button variant="contained" style={{ width: "100%"}}>Easy Apply</Button>
+                <Typography variant="body2">{minExp} - {maxExp} years</Typography>
+                <Button variant="contained" style={{ width: "100%", backgroundColor: green.A400, color: "black", textTransform: "capitalize", fontWeight: "bold", marginTop: 10}}>Easy Apply</Button>
             </CardContent>
-            <CardActions style={{ display: "flex", flexDirection: "column", alignItems: "start"}}>  
-                
-            </CardActions>
+            
         </Card>
     )
 }
